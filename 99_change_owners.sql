610a537d1329728c49f9a90e41e87391a4755930
Coen Pieterse <coen.pieterse@tnt.com>
2017-06-23 09:12:46 +0200

    SMall fixes

---
+++ 99_change_owners.sql
@@ -0,0 +1,27 @@
+ALTER TABLE stg2_customer_daily_metrics_account OWNER TO "coen.pieterse";
+ALTER TABLE stg2_fincon_daily_metrics_account OWNER TO "coen.pieterse";
+ALTER TABLE stg2_customer_metrics_account OWNER TO "coen.pieterse";
+
+ALTER TABLE stg2_Account_downtraders_monthly OWNER TO "coen.pieterse";
+ALTER TABLE stg2_Account_downtraders_monthly_exp_all1 OWNER TO "coen.pieterse";
+ALTER TABLE stg2_Account_downtraders_monthly_exp_all2 OWNER TO "coen.pieterse";
+ALTER TABLE stg2_Account_downtraders_monthly_cus OWNER TO "coen.pieterse";
+ALTER TABLE stg2_Account_downtraders_monthly_seq OWNER TO "coen.pieterse";
+ALTER TABLE stg2_Account_downtraders_monthly_seq_full OWNER TO "coen.pieterse";
+ALTER TABLE stg2_Account_downtraders_monthly_cus OWNER TO "coen.pieterse";
+ALTER TABLE stg2_Account_downtraders_monthly_cus OWNER TO "coen.pieterse";
+
+
+ALTER TABLE stg3_downtraders_sample OWNER TO "coen.pieterse";
+ALTER TABLE stg3_downtraders_sample2 OWNER TO "coen.pieterse";
+ALTER TABLE stg3_downtraders_churners_NL_DE_GR_CY_IE OWNER TO "coen.pieterse";
+ALTER TABLE stg3_list_downtraders_churners_nl_de_gr_cy_ie OWNER TO "coen.pieterse";
+ALTER TABLE stg3_customer_list OWNER TO "coen.pieterse";
+ALTER TABLE stg3_churn_downtrade_DE OWNER TO "coen.pieterse";
+ALTER TABLE stg3_churn_downtrade_NL OWNER TO "coen.pieterse";
+ALTER TABLE stg3_churn_downtrade_IE OWNER TO "coen.pieterse";
+ALTER TABLE stg3_churn_downtrade_GR OWNER TO "coen.pieterse";
+ALTER TABLE stg3_churn_downtrade_CY OWNER TO "coen.pieterse";
+ALTER TABLE stg3_churn_downtrade_BE OWNER TO "coen.pieterse";
+
+
